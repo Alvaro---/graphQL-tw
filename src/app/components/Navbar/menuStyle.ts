b2b4a7f1c6a50057ba052a5f1b233589e580ab2a
Alvaro <alvaro.mercado.valle@gmail.com>
2022-07-25 01:37:51 -0400

    landing top and navbar

---
+++ src/app/components/Navbar/menuStyle.ts
@@ -0,0 +1,47 @@
+export default {
+    bmBurgerButton: {
+        position: 'absolute',
+        width: '20px',
+        height: '20px',
+        right: '20px',
+        top: '20px'
+    },
+    bmBurgerBars: {
+        background: '#373a47'
+    },
+    bmBurgerBarsHover: {
+        background: '#a90000'
+    },
+    bmCrossButton: {
+        height: '24px',
+        width: '24px'
+    },
+    bmCross: {
+        background: '#bdc3c7'
+    },
+    bmMenuWrap: {
+        position: 'fixed',
+        top:0,
+        width:"60%",
+        height: '100%'
+    },
+    bmMenu: {
+        background: '#373a47',
+        padding: '2.5em 2.5em 0',
+        fontSize: '1.15em',
+    },
+    bmMorphShape: {
+        fill: '#373a47'
+    },
+    bmItemList: {
+        marginTop:'20px',
+        color: '#b8b7ad',
+        padding: '0.8em'
+    },
+    bmItem: {
+        display: 'inline-block'
+    },
+    bmOverlay: {
+        background: 'rgba(0, 0, 0, 0.3)'
+    }
+}
